5d0ddec1ee183463bd7dfcae249f4a257fabcbc1
Andres Eduardo Ramirez <andresoni_2000@hotmail.com>
2018-10-16 09:09:27 -0500

    Este es el quiz sobre los diferentes dispositivos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TelevisionPage } from '../television/television';
+import { BocinaPage } from '../bocina/bocina';
+import { SmartphonePage } from '../smartphone/smartphone';
+import { LamparaPage } from '../lampara/lampara';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+television=TelevisionPage;
+bocina=BocinaPage;
+smartphone=SmartphonePage;
+lampara=LamparaPage;
+
+  tele(){
+    this.navCtrl.push(this.television);
+  }
+
+  smart(){
+    this.navCtrl.push(this.smartphone);
+  }
+
+  lamp(){
+    this.navCtrl.push(this.lampara);
+  }
+
+  boc(){
+    this.navCtrl.push(this.bocina);
+  }
 
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
